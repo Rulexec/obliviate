861ed2daa6926f8098edf68a8a1acba7911a48a8
Alexander Ruliov <rulix.exec@gmail.com>
2016-11-26 13:20:27 +0300

    force https

--- frontend/build.js
+++ frontend/build.js
@@ -5,15 +5,50 @@ const STAT_COUNTERS = '%STAT_COUNTERS%';
 let indexHtml = fs.readFileSync(__dirname + '/index.html'),
     statCountersPos = indexHtml.indexOf(STAT_COUNTERS);
 
-var statCountersHtml;
+var statCountersHtml, forceHttpsHtml;
 
-if (!process.env.LOCAL) statCountersHtml = fs.readFileSync(__dirname + '/statCounters.html');
-else statCountersHtml = '';
+if (!process.env.LOCAL) {
+  statCountersHtml = fs.readFileSync(__dirname + '/statCounters.html');
+  forceHttpsHtml = "<script>if (document.location.protocol === 'http:') document.location.protocol = 'https:';</script>";
+} else {
+  statCountersHtml = '';
+  forceHttpsHtml = '';
+}
 
-let rendered = [
-  indexHtml.slice(0, statCountersPos), 
-  statCountersHtml,
-  indexHtml.slice(statCountersPos + STAT_COUNTERS.length)
-].join('');
+let replaces = {
+  STAT_COUNTERS: statCountersHtml,
+  FORCE_HTTPS: forceHttpsHtml
+};
+
+let chunks = [];
+
+for (let key of Object.keys(replaces)) {
+  let value = replaces[key];
+
+  let pos = indexHtml.indexOf(`%${key}%`);
+  if (pos === -1) continue;
+
+  chunks.push({
+    pos: pos,
+    keyLength: key.length + 2,
+    value: value
+  });
+}
+
+chunks.sort(function(a, b) { return a.pos - b.pos; });
+
+let substituted = chunks.reduce(function({result, lastPos}, {pos, keyLength, value}) {
+  result.push(indexHtml.slice(lastPos, pos));
+  lastPos = pos + keyLength;
+
+  result.push(value);
+
+  return {result, lastPos};
+}, {result: [], lastPos: 0});
+
+chunks = substituted.result;
+chunks.push(indexHtml.slice(substituted.lastPos));
+
+let rendered = chunks.join('');
 
 fs.writeFileSync(__dirname + '/static/index.html', rendered);
